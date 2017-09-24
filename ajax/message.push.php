c4525cddff22e3271106c71b36beb23347eafc90
Vyacheslav Okara <support-cs2@ya.ru>
2017-09-24 13:37:33 +0300

    create messages add func

---
+++ ajax/message.push.php
@@ -0,0 +1,44 @@
+<?php
+
+	# Автоподгрузка классов
+	function __autoload( $name ) { include( "../class/class_" . $name . ".php" ); }
+
+	# Подключаем конфиг 
+	$config = new config;
+
+	# Подключаем функции
+	$func = new func;
+
+	# Соединяемся с бд
+	$db = new db($config->HostDB, $config->UserDB, $config->PassDB, $config->BaseDB);
+
+
+	if( !empty($_POST['name']) && !empty($_POST['email']) && !empty($_POST['message']) ) {
+
+		$name = $func->TextClean($_POST['name']);
+		$message = $func->TextClean($_POST['message']);
+		$email = false;
+		$user_id = 0;
+
+		if( $func->IsMail($_POST['email']) ) {
+			$email = $_POST['email'];
+		}
+
+		if( !$email ) exit('{"status":"error"}');
+
+		$db->Query("select `id` from users where email = '$email' ");
+		$search_user_id = $db->FetchRow();
+		
+		if( $search_user_id > 0 ) { 
+			$user_id = $search_user_id;
+		}else{
+			$db->Query("insert into `users` (name, email) values ('$name', '$email') ");
+			$user_id = $db->LastInsert();
+		}
+
+		$db->Query("insert into `messages` (user_id, message, date) values ('" . $user_id . "', '$message', '" . time() . "') ");
+		echo '{"status":"success"}';
+
+	}
+
+?>
--- pages/index.php
+++ pages/index.php
@@ -5,27 +5,55 @@
 	<title>{!TITLE!}</title>
 </head>
 <body>
+	<h1>Ну эт типа записи</h1>
 	<table>
 		<tr>
 			<td>User</td>
+			<td>E-mail</td>
 			<td>Message</td>
+			<td>Date</td>
 		</tr>
 
 		<?php
 
-			$db->Query("select message.*, users.* from `message`, `users` where message.user_id = users.id ");
+			$db->Query("select messages.*, users.* from `messages`, `users` where messages.user_id = users.id ");
 
 			while ($row = $db->FetchArray()) { ?>
 				
 				<tr>
 					<td><?php echo $row['name']; ?></td>
+					<td><?php echo $row['email']; ?></td>
 					<td><?php echo $row['message']; ?></td>
+					<td><?php echo date( 'd-m-Y, H:i:s', $row['date'] ); ?></td>
 				</tr>
 
-		<?php }
-
-		?>
+		<?php }	?>
 
 	</table>
+
+	<h1>Добавить запись</h1>
+
+	<form method="post">
+		<input type="text" name="name" id="name" placeholder="Ваше имя, сударь">
+		<input type="text" name="email" id="email" placeholder="Електронная почта, мой король">
+		<textarea name="message" id="message" placeholder="message"></textarea>
+		<button>Сохранить мой прекрастный комментарий!!!</button>
+	</form>
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+	<script>
+		$('form').submit(function(event) {
+			$.ajax({
+				url: '/ajax/message.push.php',
+				type: 'post',
+				data: $(this).serialize(),
+				success: function(data) {
+					console.log(data);
+				}
+			})
+			return false;
+		});
+	</script>
+
 </body>
 </html>
